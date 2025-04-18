83b00a6500d61426b5e2a8f39bca46788a2e558b
JuneauYuki <cjquan02@gmail.com>
2025-04-18 16:50:18 -0700

    basic game

---
+++ broncohacksgame/main_scene.gd
@@ -0,0 +1,25 @@
+extends Node2D
+@onready var character: CharacterBody2D = $Character
+
+var rng
+var speed
+var count = 0
+@onready var speed_label: Label = $Speed
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	rng = RandomNumberGenerator.new()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	count += 1
+	if count > 15:
+		count = 0
+		speed = rng.randf_range(-5.0, 5.0)
+		speed = round(speed * 100) / 100.0
+		speed_label.text = "Speed: " + str(speed)
+		if speed > 2.0:
+			character.jump(speed)
+			
